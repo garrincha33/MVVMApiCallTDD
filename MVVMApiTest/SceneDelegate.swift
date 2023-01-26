839b9a7ddaad5fbff3627a721c71f9590d216bbf
Richy <garrincha33@users.noreply.github.com>
2023-01-26 17:00:20 +0000

    added test for view model count and using dependency injection for the view model in VC

--- MVVMApiTest/SceneDelegate.swift
+++ MVVMApiTest/SceneDelegate.swift
@@ -10,13 +10,13 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    private var viewModel = UserViewModel(userService: UserService.init())
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        window.rootViewController = ViewController()
+        window.rootViewController = UserTableViewController(viewModel: viewModel)
         window.makeKeyAndVisible()
     }
 
--- MVVMApiTestTests/MVVMUserTableViewControllerTests.swift
+++ MVVMApiTestTests/MVVMUserTableViewControllerTests.swift
@@ -48,34 +48,7 @@ final class MVVMUserTableViewControllerTests: XCTestCase {
         tableViewController.viewDidLoad()
         XCTAssertEqual(expected, tableView?.numberOfRows(inSection: expected))
     }
-    
-    
-
 
 }
 
-class UserTableViewController: UITableViewController {
-    
-    var viewModel: UserViewModel
-    
-    init(viewModel: UserViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .purple
-        setupTableView()
-    }
 
-    private func setupTableView() {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
-    }
-    
-    
-}
